b650d3123d13b3c2e326bbf1f3fbcade9fb86d11
Dominick <Dbattine@stevens.edu>
2021-12-04 16:37:56 -0500

    sleepLog functions
    Adds log to a list of previous logs and displays the most recent log at the top

---
+++ java.js
@@ -0,0 +1,12 @@
+function sleepLog(){
+    var text = document.getElementById('text').value;
+    document.getElementById('text').value = '';
+    console.log(text);
+    var ul = document.getElementById("posts");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(text));
+    li.setAttribute("class", "entry"); // added line
+    ul.appendChild(li);
+    document.getElementById('recent').innerHTML = text;
+
+}
